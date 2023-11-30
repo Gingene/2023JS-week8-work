1927a61cacd3c2d126ac1183faf5c971fc9011ac
Gingene <ginx2618@gmail.com>
2023-11-30 23:10:45 +0800

    fix: all js pathname

--- js/all.js
+++ js/all.js
@@ -2,10 +2,18 @@ import "./helpers/d3.min.js";
 import "./helpers/c3.min.js";
 const currentPath = window.location.pathname;
 
-if (currentPath === "/index.html") {
+if (
+  currentPath === "/" ||
+  currentPath === "/index.html" ||
+  currentPath === "/2023JS-week8-work/" ||
+  currentPath === "/2023JS-week8-work/index.html"
+) {
   import("./utils/index-animation.js");
   import("./user/user.js");
-} else if (currentPath === "/admin.html") {
+} else if (
+  currentPath === "/admin.html" ||
+  currentPath === "/2023JS-week8-work/admin.html"
+) {
   import("./utils/admin-animation.js");
   import("./admin/admin.js");
 }
